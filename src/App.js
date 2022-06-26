d5302149a472dca9a51cb3eb880412a43d21ffa1
Aviad Mizrachi <aviadm@gmail.com>
2022-06-26 15:11:26 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import {useAuth, useLoginWithRedirect} from "@frontegg/react";
+import {useEffect} from "react";
+import {ContextHolder} from "@frontegg/rest-api";
+import { gql, useLazyQuery } from '@apollo/client';
+
+const GET_LAUNCHES = gql`
+          query Launches {
+              launches {
+                mission_name
+                mission_id
+                rocket {
+                  rocket_name
+                  rocket {
+                    company
+                    name
+                    mass {
+                      kg
+                    }
+                  }
+                }
+                launch_site {
+                  site_name
+                }
+                launch_date_local
+              }
+            }
+        `;
+
+function drawLaunches(data) {
+    console.log('launches - ', data.launches);
+    return <div>
+        {data.launches.map(launch => (
+            <div key={launch.mission_name} className='station'>Launch: {launch.mission_name}</div>
+        ))}
+    </div>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const { user, isAuthenticated } = useAuth();
+
+    const loginWithRedirect = useLoginWithRedirect();const [
+        getLaunches,
+        { loading: loadingLaunches, data: launches }
+    ] = useLazyQuery(GET_LAUNCHES);
+
+    console.log('isAuthenticated - ', isAuthenticated);
+
+    const logout = () => {
+        const baseUrl = ContextHolder.getContext().baseUrl;
+        window.location.href = `${baseUrl}/oauth/logout?post_logout_redirect_uri=${window.location}`;
+    };
+
+    useEffect(() => {
+        if (!isAuthenticated) {
+            console.log('not authenticated. redirecting to login');
+            loginWithRedirect();
+        }
+    }, [isAuthenticated, loginWithRedirect]);
+
+    return (
+        <div className="App">
+            {isAuthenticated ? (
+                <div>
+                    <div>
+                        <span>Logged in as: {user?.name}</span>
+                    </div>
+                    <div>
+                        <button onClick={() => alert(user.accessToken)}>What is my access token?</button>
+                    </div>
+                    <div>
+                        {loadingLaunches ? <div>Loading some launches from SpaceX...</div> : <button onClick={() => getLaunches()}>Call my graphql server</button>}
+                    </div>
+                    <div>
+                        <button onClick={() => logout()}>Click to logout</button>
+                    </div>
+                    {launches ? drawLaunches(launches) : null}
+                </div>
+            ) : (
+                <div>Redirecting to login dialog...</div>
+            )}
+        </div>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,54 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { FronteggProvider } from '@frontegg/react';
+import { ContextHolder } from '@frontegg/rest-api';
+import { setContext } from '@apollo/client/link/context';
+
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    createHttpLink
+} from "@apollo/client";
+
+const uri = 'https://api.spacex.land/graphql';
+const httpLink = createHttpLink({
+    uri,
+});
+
+const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = ContextHolder.getAccessToken();
+    // return the headers to the context so httpLink can read them
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        }
+    }
+});
+
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: authLink.concat(httpLink),
+});
+
+const contextOptions = {
+    baseUrl: 'https://samples-demo.frontegg.com',
+    clientId: '2e53360e-517e-4c38-a040-ba0e8639f2c7'
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <FronteggProvider contextOptions={contextOptions} hostedLoginBox={true}>
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    </FronteggProvider>,
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
